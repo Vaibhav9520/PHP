169677d6a830d893d9352a6fba8eb6ea4c2f9b26
Vaibhav9520 <vaibhavsingh01080@gmail.com>
2024-09-23 11:09:30 +0530

    update

--- Pattern.php
+++ Pattern.php
@@ -1,10 +1,25 @@
 <?php
+    // $n = 5;
+    // echo "Pattern Printing: <br>";
+    // for($i=0;$i<=$n;$i++){
+    //     for($j=0;$j<$i;$j++){
+    //         echo "*";
+    //     }
+
+    //     echo "<br>";
+    // }
     $n = 5;
-    echo "Pattern Printing: <br>";
-    for($i=0;$i<=$n;$i++){
-        for($j=0;$j<$i;$j++){
-            echo "*";
+
+    echo "Left Pattern Printing: <br>";
+    for ($i = 1; $i <= $n; $i++) {
+        // Print spaces
+        for ($j = 1; $j <= $n - $i; $j++) {
+            echo "&nbsp";
         }
-        echo "<br>";
+        // Print stars
+        for ($j = 1; $j <= $i; $j++) {
+            echo "A";
+        }
+        echo "<br>"; 
     }
 ?>
