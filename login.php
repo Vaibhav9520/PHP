66c96eef5bec96570fd048688d7891dfab26bc56
Vaibhav9520 <vaibhavsingh01080@gmail.com>
2024-10-14 10:18:33 +0530

    commit

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+$valid_username = 'Vaibhav';
+$valid_password = '123';
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+if ($username === $valid_username && $password === $valid_password) {
+    echo "Login successful! Welcome, $username.";
+} else {
+    echo "Invalid username or password. Please try again.";
+}
+?>
