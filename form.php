21a0118a2046fd6f7028eae11d77f72229da9476
Vaibhav Singh <119606722+Vaibhav9520@users.noreply.github.com>
2024-10-17 22:43:10 +0530

    Update form.php

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']); 
 
-    
+
     if (!empty($name)) {
         echo "<h2>Hello, " . $name . "!</h2>";
     } else {
